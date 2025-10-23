871b904b19c3759ae313c4983ac35a07b7ee1b18
choROPeNt <christian.duereth@tu-dresden.de>
2025-10-23 16:28:38 +0200

    moved h5 tools to addons

--- sam2/addons/__init__.py
+++ sam2/addons/__init__.py
@@ -1,9 +1,11 @@
 
 from .vizualization import show_anns, overlay_from_instances
 from .patcher import Sam2Patcher
+from .h5tools import write_h5
 
 __all__ = [
     "Sam2Patcher",
     "show_anns",
-    "overlay_from_instances"
+    "overlay_from_instances",
+    "write_h5"
     ]
