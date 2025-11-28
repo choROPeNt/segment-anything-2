19d233fb8877a499a40ac7e1866a6339f2839662
choROPeNt <christian.duereth@tu-dresden.de>
2025-11-28 13:40:08 +0100

    added everything from descriptors

--- sam2/addons/__init__.py
+++ sam2/addons/__init__.py
@@ -2,7 +2,7 @@
 from .vizualization import show_anns, overlay_from_instances
 from .patcher import Sam2Patcher
 from .h5tools import write_h5, read_h5
-from .descriptors import s2_descriptor, phi_descriptor, rve_size_from_integral_range, integral_range_from_S2r
+from .descriptors import s2_descriptor, phi_descriptor, rve_size_from_integral_range, integral_range_from_S2r, corr_length_halfheight
 
 __all__ = [
     "Sam2Patcher",
@@ -13,5 +13,6 @@ __all__ = [
     "s2_descriptor",
     "phi_descriptor",
     "rve_size_from_integral_range",
-    "integral_range_from_S2r"
+    "integral_range_from_S2r",
+    "corr_length_halfheight"
     ]
